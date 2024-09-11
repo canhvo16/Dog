479f61015b16e3cb2cac45edc8f30baaa2e222ff
Canh Vo <canhvo16@gmail.com>
2024-09-10 20:22:11 -0500

    added function to get dog images after selecting breed

--- dogs/src/App.js
+++ dogs/src/App.js
@@ -4,20 +4,30 @@ import axios from 'axios';
 
 function App() {
   const [breeds, setBreeds] = useState(null)
+  const [selectedBreed, setSelectedBreed] = useState(null)
+  const [dogImages, setDogImages] = useState(null)
+
+  const getBreedImages = (breed) => {
+    const url = `https://dog.ceo/api/breed/${breed}/images`
+    axios.get(url).then((response) => {setDogImages(response.data.message)})
+  }
 
   useEffect(() => {
     const url = "https://dog.ceo/api/breeds/list/all"
     axios.get(url).then((response) => {setBreeds(response.data.message)})
-    console.log(breeds)
   }, [])
 
   return (
     <div>
-      <select>
+      <select onChange={(e) => {
+        setSelectedBreed(e.target.value)
+        getBreedImages(selectedBreed)
+      }}>
         {breeds ? Object.keys(breeds).map((breed, i) => {
           return <option key={breed} value={breed}>{breed}</option>
         }) : null}
       </select>
+      
     </div>
   );
 }
