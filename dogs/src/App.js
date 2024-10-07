641f2894cdd237bb7ada1ddad62a7509b2bc12a3
Canh Vo <canhvo16@gmail.com>
2024-10-07 18:19:06 -0500

    editted function names, removed redundant code, altered HTML structure, and added some CSS

--- dogs/src/App.js
+++ dogs/src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [dogImages, setDogImages] = useState(null)
   const [showAll, setShowAll] = useState(false)
 
-  const onChange = (e) => {
+  const setDogBreed = (e) => {
     if (e.target.value !== 'default') {
     getBreedImages(e.target.value)
     } else {
@@ -17,8 +17,7 @@ function App() {
   }
 
   const getBreedImages = (breed) => {
-    const url = `https://dog.ceo/api/breed/${breed}/images`
-    axios.get(url).then((response) => {setDogImages(response.data.message)})
+    axios.get(`https://dog.ceo/api/breed/${breed}/images`).then((response) => {setDogImages(response.data.message)})
   }
 
   const clearSelection = () => {
@@ -29,27 +28,29 @@ function App() {
   }
 
   useEffect(() => {
-    const url = "https://dog.ceo/api/breeds/list/all"
-    axios.get(url).then((response) => {setBreeds(response.data.message)})
+    axios.get("https://dog.ceo/api/breeds/list/all").then((response) => {setBreeds(response.data.message)})
   }, [])
 
   return (
     <div>
-      <select onChange={onChange} id='dropdown'>
+      <h1 id='header'>Dog Breed Images</h1>
+      <select onChange={setDogBreed} id='dropdown'>
         <option value="default">Choose a breed!</option>
         {breeds ? Object.keys(breeds).map((breed, i) => {
           return <option key={breed} value={breed}>{breed}</option>
         }) : null}
       </select>
-      <div>
+      <div id='imageGallery'>
         {dogImages && showAll? dogImages.map((image, i) => {
-          return <img src={image} alt={image} key={image}></img> 
+          return <img className='dogImage' src={image} alt={image} key={image}></img> 
           }) : null}
         {dogImages && showAll === false ? dogImages.slice(0,10).map((image, i) => {
-          return <img src={image} alt={image} key={image}></img> 
+          return <img className='dogImage' src={image} alt={image} key={image}></img> 
           }) : null}
-        {dogImages ? <button onClick={() => setShowAll(!showAll)}>{showAll ? "Show Less Images" : "Show All Images"}</button> : null}
-        {showAll ? <button onClick={() => clearSelection()}>Clear Selection</button> : null}
+      </div>
+      <div>
+        {dogImages ? <button className='button' onClick={() => setShowAll(!showAll)}>{showAll ? "Show Less Images" : "Show All Images"}</button> : null}
+        {dogImages ? <button className='button' onClick={() => clearSelection()}>Clear Selection</button> : null}
       </div>
     </div>
   );
